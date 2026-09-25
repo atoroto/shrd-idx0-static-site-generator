d972981afec9475a5d81acdb0f8e9af56f1673fd
atoroto <9368370+atoroto@users.noreply.github.com>
2026-09-25 21:22:50 +0300

    textnode

---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text  # The text content of the node
+        self.text_type = text_type  # The type of text this node contains, which is a member of the TextType enum.
+        self.url = url  # The URL of the link or image, if the text is a link. Default to None if nothing is passed in.
+
+    def __eq__(self, other):
+        return bool(
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url}"
